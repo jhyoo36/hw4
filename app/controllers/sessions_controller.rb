6d02445b2366b007aa3c9bce4ed3530088646ef1
James Yoo <james.yoo@kellogg.northwestern.edu>
2025-03-09 03:35:48 +0000

    Firstpass

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,41 @@
 class SessionsController < ApplicationController
   def new
+    # render login form in sessions/new.html.erb
   end
 
   def create
+    # authenticate the user
+    # 1. try to find the user by their unique identifier
+    @user = User.find_by({"email" => params["email"]})
+    # 2. if the user exists -> check if they know their password
+    if @user != nil
+      # 3. if they know their password -> login is successful
+      # if @user["password"] == params["password"]
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+
+        # --- begin user session
+        session["user_id"] = @user["id"]
+
+        flash["notice"] = "Welcome."
+        redirect_to "/"
+      else
+        flash["notice"] = "Nope."
+        redirect_to "/login"
+      end
+      # 4. if the user doesn't exist or they don't know their password -> login fails
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    # logout the user
+    session["user_id"] = nil
+
+    flash["notice"] = "Goodbye."
+    redirect_to "/"
   end
 end
-  
+
+
