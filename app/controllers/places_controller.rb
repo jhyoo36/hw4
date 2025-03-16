e5d1049c68c11d661414ea49d677ca65d87570ef
James Yoo <james.yoo@kellogg.northwestern.edu>
2025-03-16 23:13:20 +0000

    only own entries view

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,8 +6,9 @@ class PlacesController < ApplicationController
   end
 
   def show
+    @user = User.find_by({ "id" => session["user_id"] })
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"]})
+    @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @user["id"]})
   end
 
   def new
